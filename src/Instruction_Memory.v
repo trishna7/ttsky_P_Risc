0e2c4118007d151862fbfb5fe716ec7201278797
trishna7 <trishna722.ts33@gmail.com>
2025-08-11 08:31:19 +0545

    no initiam in im

--- src/Instruction_Memory.v
+++ src/Instruction_Memory.v
@@ -10,25 +10,25 @@ module Instruction_Memory (
     reg [31:0] memory [0:14];
 
     // Initial load for simulation and Tiny Tapeout submission
-    initial begin
-        // for (integer i = 0; i < 20; i = i + 1)
-        //     memory[i] = 32'b0;
-          memory[0] = 32'h00100093;
-        memory[1] = 32'h80000337;
-        memory[2] = 32'h00000013;
-        memory[3] = 32'h00000013;
-        memory[4] = 32'h00132023;
-        memory[5] = 32'h00000013;
-        memory[6] = 32'h00200093;
-        memory[7] = 32'h00132023;
-        memory[8] = 32'h00000013;
-        memory[9] = 32'h00000093;
-        memory[10] = 32'h00132023;
-        memory[11] = 32'h00100093;
-        memory[12] = 32'h00132023;
-        memory[13] = 32'hfe000ae3;
-        // Remaining locations already initialized to 0
-    end
+    // initial begin
+    //     // for (integer i = 0; i < 20; i = i + 1)
+    //     //     memory[i] = 32'b0;
+    //       memory[0] = 32'h00100093;
+    //     memory[1] = 32'h80000337;
+    //     memory[2] = 32'h00000013;
+    //     memory[3] = 32'h00000013;
+    //     memory[4] = 32'h00132023;
+    //     memory[5] = 32'h00000013;
+    //     memory[6] = 32'h00200093;
+    //     memory[7] = 32'h00132023;
+    //     memory[8] = 32'h00000013;
+    //     memory[9] = 32'h00000093;
+    //     memory[10] = 32'h00132023;
+    //     memory[11] = 32'h00100093;
+    //     memory[12] = 32'h00132023;
+    //     memory[13] = 32'hfe000ae3;
+    //     // Remaining locations already initialized to 0
+    // end
 
     // Synchronous write for UART programming
     always @(posedge CLK) begin
